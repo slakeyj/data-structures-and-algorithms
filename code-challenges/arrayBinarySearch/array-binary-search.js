548b516ad104ab2f5df795bcfc7dd8df7b0eec9d
Susanna Lakey <susannalakey@gmail.com>
2020-02-16 12:47:23 -0800

    updating changes to binary search

--- code-challenges/arrayBinarySearch/array-binary-search.js
+++ code-challenges/arrayBinarySearch/array-binary-search.js
@@ -1,22 +1,30 @@
-const BinarySearch = (arr, searchKey) => {
-
-  let start;
+const binarySearch = (arr, searchKey) => {
+  let start = 0;
   let end = arr.length - 1
-  let midPoint = end - start / 2;
-  console.log('arr mid', arr[midPoint])
-  if (arr[midPoint] === searchKey) {
+  let midPoint = (end - start) / 2;
+  if (searchKey === arr[midPoint]) {
     return midPoint;
   }
   while (searchKey !== arr[midPoint]) {
-    if (searchKey < arr[midpoint]) {
-      end = midPoint;
-    }
-    if (searchKey > arr[midPoint]) {
-      start = midPoint;
+    if (searchKey < arr[midPoint]) {
+      end = midPoint - 1;
+
+      midPoint = Math.floor((end - start) / 2) - midPoint;
+      console.log('state:', start, midPoint, end);
+    } else if (searchKey > arr[midPoint]) {
+      start = midPoint + 1;
+      midPoint = Math.floor((end - start) / 2) + midPoint;
+      console.log('state:', start, midPoint, end);
+    } else {
+      console.log('state:', start, midPoint, end);
+      if (arr[midPoint] === searchKey) {
+        return midPoint;
+      }
+      break;
     }
   }
-  return -1;
+  return -1
 }
 
 
-module.exports = BinarySearch;
+module.exports = binarySearch;
