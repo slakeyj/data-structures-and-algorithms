21d1efbeb8fefd503ddc6ea54c1097d409359ff1
Susanna Lakey <susannalakey@gmail.com>
2020-02-24 15:28:58 -0800

    file structure in place

---
+++ code-challenges/queueWithStacks/queue-with-stacks.test.js
@@ -0,0 +1,19 @@
+const Stack = require('../../')
+
+
+
+
+class PseudoQueue {
+  constructor() {
+
+  }
+
+
+  enqueue(value) {
+    // inserts first in first out
+  }
+
+  dequeue() {
+    // extracts value using first-in, first-out
+  }
+}
