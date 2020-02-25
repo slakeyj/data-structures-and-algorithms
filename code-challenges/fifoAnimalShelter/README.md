4ae9f926a01727977023834452d971d7b327c0d4
Susanna Lakey <susannalakey@gmail.com>
2020-02-25 14:28:58 -0800

    enqueue complete, working on dequeue

---
+++ code-challenges/fifoAnimalShelter/README.md
@@ -0,0 +1,13 @@
+# FIFO Animal Shelter
+* Create a class called Animal shelter with methods enqueue and dequeue
+
+## Challenge Description
+* Create a class AnimalShelter which holds only dogs and cats. Operates using a First-in, First-Out approach.
+* Implement enqueue(anima). Adds an animal to the shelter, can be either a cat or dog.
+* Implement dequeue(pref). Returns either a dog or a cat.  If they are not a cat or dog, return null.
+
+## Approach & Efficiency
+* I haven't solved this problem at this time. 
+
+## Solution
+ ![Whiteboard](../../assets/fifo-animal-shelter.jpg)
---
+++ code-challenges/fifoAnimalShelter/fifo-animal-shelter.test.js
@@ -0,0 +1,45 @@
+const AnimalShelter = require('./fifo-animal-shelter');
+
+describe('AnimalShelter', () => {
+  let animalShelter;
+  beforeEach(() => {
+    animalShelter = new AnimalShelter();
+  })
+  describe('enqueue', () => {
+    it('increases the length of the storage', () => {
+      expect(animalShelter.enqueue('dog')).toEqual(animalShelter.storage.length);
+    })
+
+    // revisit this test
+    it('throws an error when given an invalid input', () => {
+      expect(animalShelter.enqueue('rabbit')).toEqual('Not a valid input');
+    })
+
+    it('adds the value to this.storage', () => {
+      animalShelter.enqueue('dog');
+      expect(animalShelter.storage).toEqual(['dog'])
+    })
+  })
+
+  describe('dequeue', () => {
+    beforeEach(() => {
+      animalShelter.enqueue('cat');
+      animalShelter.enqueue('dog');
+      animalShelter.enqueue('cat');
+
+    })
+    it('returns null if the pref is not found in storage', () => {
+      expect(animalShelter.dequeue('rabbit')).toEqual(null);
+    })
+    it('removes the last instance of the given pref', () => {
+      animalShelter.dequeue('dog');
+      expect(animalShelter.storage).toEqual(['cat', 'cat'])
+    })
+    it('can dequeue multiple items in a row', () => {
+      animalShelter.dequeue('dog');
+      animalShelter.dequeue('cat');
+      animalShelter.dequeue('cat');
+      expect(this.storage).toEqual([]);
+    })
+  })
+})
