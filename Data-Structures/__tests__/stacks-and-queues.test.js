0c3f035f11486c211f7a9f7fe5b351e7bcc08559
Susanna Lakey <susannalakey@gmail.com>
2020-02-24 18:52:14 -0800

    reverted everything back to just array, got rid of all use of nodes because it didn't make sense

--- Data-Structures/__tests__/stacks-and-queues.test.js
+++ Data-Structures/__tests__/stacks-and-queues.test.js
@@ -4,8 +4,7 @@ describe('Stack', () => {
 
   it('can successfully instantiate an empty stack', () => {
     const testStack = new Stack();
-    expect(testStack.storage.length).toEqual(0);
-    expect(testStack.top).toEqual(null);
+    expect(testStack.length()).toEqual(0);
   })
 
   describe('peek()', () => {
@@ -19,15 +18,21 @@ describe('Stack', () => {
     })
 
     describe('peek() when storage is populated', () => {
-      const testStack = new Stack();
-      testStack.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+        testStack.push(5);
+        testStack.push(4);
+      });
 
       it('returns the top value of a stack', () => {
-        expect(testStack.peek()).toEqual(testStack.storage[0].value);
+        expect(testStack.peek()).toEqual(4);
       })
 
       it('does not change the length of the storage', () => {
-        expect(testStack.storage.length).toEqual(2);
+        testStack.peek();
+        expect(testStack.length()).toEqual(2);
       })
     })
   })
@@ -35,15 +40,20 @@ describe('Stack', () => {
 
   describe('push()', () => {
     describe('push() when storage is populated', () => {
-      const testStack = new Stack();
-      testStack.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+        testStack.push(5);
+        testStack.push(4);
+      })
+
       it('returns the new length of the array', () => {
-        expect(testStack.push(3)).toEqual(testStack.storage.length);
+        expect(testStack.push(3)).toEqual(testStack.length());
       })
 
       it('adds the new value to the top of the stack', () => {
-
-        expect(testStack.storage[0].value).toEqual(3);
+        expect(testStack.peek()).toEqual(4);
       })
     })
   })
@@ -51,57 +61,82 @@ describe('Stack', () => {
 
   describe('pop()', () => {
     describe('pop() when storage is populated', () => {
-      const testStack = new Stack();
-      testStack.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+        testStack.push(5);
+        testStack.push(4);
+      })
 
       it('removes and returns the top item in a stack', () => {
         expect(testStack.pop()).toEqual(4);
       })
 
       it('decreases the size of the stack by one', () => {
-        expect(testStack.storage.length).toEqual(1);
+        testStack.pop();
+        expect(testStack.length()).toEqual(1);
       })
     })
 
 
     describe('pop() when storage is empty', () => {
-      const testStack = new Stack();
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+      });
 
       it('returns undefined on an empty stack', () => {
-        expect(testStack.storage.pop()).toEqual(undefined)
+        expect(testStack.pop()).toEqual(undefined)
       })
 
       it('does not alter the length of the stack', () => {
-        expect(testStack.storage.length).toEqual(0);
+        expect(testStack.length()).toEqual(0);
       })
     })
 
     describe('emptying an array with pop()', () => {
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+        testStack.push(5);
+        testStack.push(4);
+      })
+
       it('can successfully empty an array after multiple pops', () => {
-        const testStack = new Stack();
-        testStack.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
         testStack.pop();
         testStack.pop();
-        expect(testStack.storage.length).toEqual(0);
-        expect(testStack.storage[0]).toBeUndefined();
+        expect(testStack.length()).toEqual(0);
       })
     })
   })
 
   describe('isEmpty()', () => {
 
-    describe('isEmpty() when the stack is populated', () => {
-      const testStack = new Stack();
-      testStack.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
-      it('returns true', () => {
-        expect(testStack.isEmpty()).toBeTruthy();
+    describe('when the stack is populated', () => {
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+        testStack.push(5);
+        testStack.push(4);
+      })
+
+      it('returns false', () => {
+        expect(testStack.isEmpty()).toBeFalsy();
       })
     })
 
-    describe('isEmpty() when the stack is empty', () => {
-      const testStack = new Stack();
-      it('returns false', () => {
-        expect(testStack.isEmpty()).toBeFalsy();
+    describe('when the stack is empty', () => {
+      let testStack;
+
+      beforeEach(() => {
+        testStack = new Stack();
+      })
+      it('returns true', () => {
+        expect(testStack.isEmpty()).toBeTruthy();
       })
     })
   })
@@ -112,29 +147,25 @@ describe('Stack', () => {
 describe('Queue', () => {
   it('can successfully instantiate an empty queue', () => {
     const testQueue = new Queue();
-    expect(testQueue.storage.length).toEqual(0);
+    expect(testQueue.length()).toEqual(0);
   })
 
   describe('enqueue', () => {
-    const testQueue = new Queue();
-    testQueue.storage = [1, 2, 3];
+    let testQueue;
+
+    beforeEach(() => {
+      testQueue = new Queue();
+      testQueue.enqueue(1);
+      testQueue.enqueue(2);
+      testQueue.enqueue(3);
+    })
 
     it('returns the new length of the queue', () => {
       expect(testQueue.enqueue(2)).toEqual(4);
     })
 
-    it('pushes the value to the end of the queue', () => {
-      expect(testQueue.storage).toEqual([1, 2, 3, { "next": null, "value": 2 }])
-    })
-
-    it('can successfully enqueue multiple values into a queue', () => {
-      const testQueue = new Queue();
-      testQueue.storage = [1, 2, 3];
-      // console.log('testqueue storage before', testQueue.storage)
-      testQueue.enqueue(4);
-      testQueue.enqueue(5);
-      // console.log('test queue storage after enquque', testQueue.storage)
-      expect(testQueue.storage).toEqual([1, 2, 3, { "next": null, "value": 4 }, { "next": null, "value": 5 }])
+    it('pushes the value into the queue', () => {
+      expect(testQueue.length()).toEqual(3);
     })
   })
 
@@ -142,57 +173,75 @@ describe('Queue', () => {
   describe('dequeue', () => {
 
     describe('dequeue when the queue is populated', () => {
-      const testQueue = new Queue();
-      testQueue.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
+      let testQueue;
+
+      beforeEach(() => {
+        testQueue = new Queue();
+        testQueue.enqueue(4);
+        testQueue.enqueue(5);
+      })
 
       it('returns the dequeued value', () => {
         expect(testQueue.dequeue()).toEqual(4);
       })
 
       it('removes the value at the front of the queue and shifts the other values down an index', () => {
-        expect(testQueue.storage[0]).toEqual({ "next": null, "value": 5 });
+        testQueue.dequeue();
+        expect(testQueue.length()).toEqual(1);
       })
 
       it('can successfully empty a queue after multiple dequeues', () => {
-        const testQueue = new Queue();
-        testQueue.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
         testQueue.dequeue();
         testQueue.dequeue();
-        expect(testQueue.storage.length).toEqual(0);
-        expect(testQueue.storage).toEqual([]);
+        expect(testQueue.length()).toEqual(0);
       })
     })
 
     describe('dequeue when the queue is not populated', () => {
-      const testQueue = new Queue();
+      let testQueue;
+
+      beforeEach(() => {
+        testQueue = new Queue();
+      });
 
       it('returns undefined', () => {
         expect(testQueue.dequeue()).toEqual(undefined);
       })
 
       it('has a length of 0', () => {
-        expect(testQueue.storage.length).toEqual(0);
+        expect(testQueue.length()).toEqual(0);
       })
     })
   })
 
   describe('peek()', () => {
 
-    describe('peek() when the queue is populated', () => {
-      const testQueue = new Queue();
-      testQueue.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
+    describe('when the queue is populated', () => {
+      let testQueue;
+
+      beforeEach(() => {
+        testQueue = new Queue();
+        testQueue.enqueue(4);
+        testQueue.enqueue(5);
+      });
 
       it('returns the first node value in the queue', () => {
-        expect(testQueue.peek()).toEqual(testQueue.storage[0].value);
+        expect(testQueue.peek()).toEqual(4);
       })
 
       it('does not alter the length of the queue', () => {
-        expect(testQueue.storage.length).toEqual(2);
+        testQueue.peek();
+        expect(testQueue.length()).toEqual(2);
       })
     })
 
-    describe('peek() when the queue is empty', () => {
-      const testQueue = new Queue();
+    describe('when the queue is empty', () => {
+      let testQueue;
+
+      beforeEach(() => {
+        testQueue = new Queue();
+      });
+
       it('should return undefined', () => {
         expect(testQueue.peek()).toBeUndefined();
       })
@@ -201,18 +250,29 @@ describe('Queue', () => {
 
   describe('isEmpty()', () => {
 
-    describe('isEmpty() when the queue is populated', () => {
-      const testQueue = new Queue();
-      testQueue.storage = [{ "next": null, "value": 4 }, { "next": null, "value": 5 }];
-      it('returns true', () => {
-        expect(testQueue.isEmpty()).toBeTruthy();
+    describe('when the queue is populated', () => {
+      let testQueue;
+
+      beforeEach(() => {
+        testQueue = new Queue();
+        testQueue.enqueue(4);
+        testQueue.enqueue(5);
+      });
+
+      it('returns false', () => {
+        expect(testQueue.isEmpty()).toBeFalsy();
       })
     })
 
-    describe('isEmpty() when the queue is empty', () => {
-      const testQueue = new Queue();
-      it('returns false', () => {
-        expect(testQueue.isEmpty()).toBeFalsy();
+    describe('when the queue is empty', () => {
+      let testQueue;
+
+      beforeEach(() => {
+        testQueue = new Queue();
+      });
+
+      it('returns true', () => {
+        expect(testQueue.isEmpty()).toBeTruthy();
       })
     })
   })
