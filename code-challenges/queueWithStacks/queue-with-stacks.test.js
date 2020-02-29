0cf7ad1672fbf5fc6edf78aca069d8e52389c69c
Susanna Lakey <susannalakey@gmail.com>
2020-02-28 20:15:31 -0800

    updating

--- code-challenges/queueWithStacks/queue-with-stacks.test.js
+++ code-challenges/queueWithStacks/queue-with-stacks.test.js
@@ -2,14 +2,14 @@ const PseudoQueue = require('./queue-with-stacks')
 
 
 
-// xdescribe('PseudoQueue', () => {
-// //   let pseudoQueue
-// //   beforeEach(() => {
-// //     pseudoQueue = new PseudoQueue();
-// //     pseudoQueue.enqueue(1);
-// //     pseudoQueue.enqueue(2);
-// //     pseudoQueue.enqueue(3);
-// //   })
+describe('PseudoQueue', () => {
+  // let pseudoQueue
+  // beforeEach(() => {
+  //   pseudoQueue = new PseudoQueue();
+  //   pseudoQueue.enqueue(1);
+  //   pseudoQueue.enqueue(2);
+  //   pseudoQueue.enqueue(3);
+})
 
 
 //   // xit('can enqueue values using Stacks push()', () => {
