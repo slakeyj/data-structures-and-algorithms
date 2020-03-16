89fc970bf84fcbf4a77646c5e381bf912852c16b
Susanna Lakey <susannalakey@gmail.com>
2020-03-16 14:56:49 -0700

    updated readme

--- Data-Structures/insertionSort/insertion-sort.js
+++ Data-Structures/insertionSort/insertion-sort.js
@@ -1,15 +1,3 @@
-// InsertionSort(int[] arr)
-
-//     FOR i = 1 to arr.length
-
-//       int j <-- i - 1
-//       int temp <-- arr[i]
-
-//       WHILE j >= 0 AND temp < arr[j]
-//         arr[j + 1] <-- arr[j]
-//         j <-- j - 1
-
-//       arr[j + 1] <-- temp
 
 
 const insertionSort = arr => {
